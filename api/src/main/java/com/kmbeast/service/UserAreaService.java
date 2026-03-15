85561260529089f6fdd2dad3a2bbcd9f2ed5af26
MING <MX2582558078@gmail.com>
2026-03-15 13:01:00 +0800

    7 用户常居住地实现-后端 注：同时还修改了application.yml中的数据库名，原本应该在脚手架时就修改

---
+++ api/src/main/java/com/kmbeast/service/UserAreaService.java
@@ -0,0 +1,22 @@
+package com.kmbeast.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.kmbeast.pojo.api.Result;
+import com.kmbeast.pojo.dto.AreaQueryDto;
+import com.kmbeast.pojo.dto.UserAreaQueryDto;
+import com.kmbeast.pojo.entity.UserArea;
+
+import java.util.List;
+
+/**
+ * 用户常居住地业务逻辑接口
+ */
+public interface UserAreaService extends IService<UserArea> {
+
+    Result<List<UserArea>> list(UserAreaQueryDto userAreaQueryDto);
+
+    Result<String> update(UserArea userArea);
+
+    Result<String> saveEntity(UserArea userArea);
+
+}
