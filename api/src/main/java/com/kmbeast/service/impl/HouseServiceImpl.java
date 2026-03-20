cdc0d1099a2366133e1a61e0f8aa58c339a6f0e7
MING <MX2582558078@gmail.com>
2026-03-21 00:26:12 +0800

    20 房屋功能实现-后端（下）

--- api/src/main/java/com/kmbeast/service/impl/HouseServiceImpl.java
+++ api/src/main/java/com/kmbeast/service/impl/HouseServiceImpl.java
@@ -1,21 +1,36 @@
 package com.kmbeast.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.kmbeast.context.LocalThreadHolder;
 import com.kmbeast.mapper.HouseMapper;
+import com.kmbeast.mapper.LandlordMapper;
+import com.kmbeast.pojo.api.ApiResult;
 import com.kmbeast.pojo.api.Result;
 import com.kmbeast.pojo.dto.HouseQueryDto;
+import com.kmbeast.pojo.dto.LandlordQueryDto;
+import com.kmbeast.pojo.em.HouseDepositEnum;
+import com.kmbeast.pojo.em.HouseDirectionEnum;
+import com.kmbeast.pojo.em.HouseFitmentEnum;
 import com.kmbeast.pojo.entity.House;
 import com.kmbeast.pojo.vo.HouseListItemVO;
+import com.kmbeast.pojo.vo.LandlordVO;
 import com.kmbeast.service.HouseService;
+import com.kmbeast.utils.AssertUtils;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * 房屋业务逻辑接口实现类
  */
 @Service
 public class HouseServiceImpl extends ServiceImpl<HouseMapper, House> implements HouseService {
+
+    @Resource
+    private LandlordMapper landlordMapper;
 
     /**
      * 房屋列表查询
@@ -25,7 +40,26 @@ public class HouseServiceImpl extends ServiceImpl<HouseMapper, House> implements
      */
     @Override
     public Result<List<HouseListItemVO>> list(HouseQueryDto houseQueryDto) {
-        return null;
+        List<HouseListItemVO> houseListItemVOS = this.baseMapper.list(houseQueryDto);
+        Integer count = this.baseMapper.listCount(houseQueryDto);
+        for (HouseListItemVO houseListItemVO : houseListItemVOS) {
+            // 通过朝向ID，设置朝向文本字样
+            if (Objects.nonNull(houseListItemVO.getDirectionId())) {
+                String detail = HouseDirectionEnum.getDetail(houseListItemVO.getDirectionId());
+                houseListItemVO.setDirectionName(detail);
+            }
+            // 通过押金方式ID，设置押金方式文本字样
+            if (Objects.nonNull(houseListItemVO.getDepositMethodId())) {
+                String detail = HouseDepositEnum.getDetail(houseListItemVO.getDepositMethodId());
+                houseListItemVO.setDepositMethodName(detail);
+            }
+            // 通过装修状态ID，设置装修状态文本字样
+            if (Objects.nonNull(houseListItemVO.getFitmentStatusId())) {
+                String detail = HouseFitmentEnum.getDetail(houseListItemVO.getFitmentStatusId());
+                houseListItemVO.setFitmentStatusName(detail);
+            }
+        }
+        return ApiResult.success(houseListItemVOS, count);
     }
 
     /**
@@ -36,7 +70,38 @@ public class HouseServiceImpl extends ServiceImpl<HouseMapper, House> implements
      */
     @Override
     public Result<String> update(House house) {
-        return null;
+        paramJudge(house);
+        updateById(house);
+        return ApiResult.success("房屋信息修改成功");
+    }
+
+    /**
+     * 参数校验
+     *
+     * @param house 房屋实体信息
+     */
+    private void paramJudge(House house) {
+        AssertUtils.hasText(house.getName(), "房屋名称不能为空");
+        AssertUtils.hasText(house.getCover(), "房屋封面不能为空");
+        AssertUtils.hasText(house.getCovers(), "房屋实况图不能为空");
+        AssertUtils.notNull(house.getTypeId(), "请设置房屋类型");
+        AssertUtils.notNull(house.getSizeNumber(), "请填写房屋产权面积");
+        AssertUtils.notNull(house.getDirectionId(), "请设置房屋朝向");
+        AssertUtils.notNull(house.getSizedId(), "请设置户型");
+        AssertUtils.notNull(house.getRent(), "请设置租金");
+        AssertUtils.notNull(house.getDepositMethodId(), "请设置押金方式");
+        AssertUtils.notNull(house.getIsSubway(), "请设置是否临近地铁");
+        if (house.getIsSubway()) { // 一旦你设置临近地铁，就要补充线路
+            AssertUtils.notNull(house.getSubwayLine(), "请设置地铁线路");
+        }
+        AssertUtils.notNull(house.getFitmentStatusId(), "请设置装修类型");
+        AssertUtils.notNull(house.getRentalType(), "请设置租赁方式");
+        // 名称长度显示
+        AssertUtils.isTrue(house.getName().length() < 30, "房屋名称最多30个字"); // 名称长度校验
+        // 实况图上传数量显示
+        String covers = house.getCovers();
+        long count = covers.chars().filter(c -> c == ',').count();
+        AssertUtils.isTrue(count < 6, "实况图最多上传6张");
     }
 
     /**
@@ -47,6 +112,17 @@ public class HouseServiceImpl extends ServiceImpl<HouseMapper, House> implements
      */
     @Override
     public Result<String> saveEntity(House house) {
-        return null;
+        paramJudge(house);
+        // 设置上当前新增的房东ID
+        LandlordQueryDto landlordQueryDto = new LandlordQueryDto();
+        landlordQueryDto.setUserId(LocalThreadHolder.getUserId()); // 设置上当前新增房屋信息的房东信息ID
+        List<LandlordVO> landlordVOS = landlordMapper.list(landlordQueryDto);
+        AssertUtils.isFalse(landlordVOS.isEmpty(), "房东信息异常，非法操作");
+        LandlordVO landlordVO = landlordVOS.get(0); // 用户自己申请的房东信息
+        AssertUtils.isTrue(landlordVO.getIsAudit(), "房东认证信息待审核中，请稍后再试");
+        house.setLandlordId(landlordVO.getId()); // 认证通过，设置上查出来的房东ID
+        house.setCreateTime(LocalDateTime.now()); // 设置上当前的操作时间
+        save(house);
+        return ApiResult.success("房屋新增成功");
     }
 }
