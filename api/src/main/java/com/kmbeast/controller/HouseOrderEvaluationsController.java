151ef38155e9621245cdb0a0b2a83fd464c566ad
MING <MX2582558078@gmail.com>
2026-04-08 23:02:10 +0800

    43 预约看房评价-后端

---
+++ api/src/main/java/com/kmbeast/controller/HouseOrderEvaluationsController.java
@@ -0,0 +1,57 @@
+package com.kmbeast.controller;
+
+import com.kmbeast.aop.Pager;
+import com.kmbeast.pojo.api.ApiResult;
+import com.kmbeast.pojo.api.Result;
+import com.kmbeast.pojo.dto.HouseOrderEvaluationsQueryDto;
+import com.kmbeast.pojo.entity.HouseOrderEvaluations;
+import com.kmbeast.pojo.vo.HouseOrderEvaluationsVO;
+import com.kmbeast.service.HouseOrderEvaluationsService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * 预约看房评价控制器
+ */
+@RestController
+@RequestMapping("/house-order-evaluations")
+public class HouseOrderEvaluationsController {
+
+    @Resource
+    private HouseOrderEvaluationsService houseOrderEvaluationsService;
+
+    /**
+     * 新增预约看房评价信息
+     */
+    @PostMapping(value = "/save")
+    @ResponseBody
+    public Result<String> save(@RequestBody HouseOrderEvaluations houseOrderEvaluations) {
+        return houseOrderEvaluationsService.saveEntity(houseOrderEvaluations);
+    }
+
+    /**
+     * 删除预约看房评价信息
+     */
+    @DeleteMapping(value = "/{id}")
+    @ResponseBody
+    public Result<String> delete(@PathVariable Integer id) {
+        houseOrderEvaluationsService.removeById(id);
+        return ApiResult.success("预约看房评价删除成功");
+    }
+
+    /**
+     * 查询预约看房评价信息
+     *
+     * @param queryDto 查询参数
+     * @return Result<List < HouseOrderEvaluationsVO>> 响应结果
+     */
+    @Pager
+    @PostMapping(value = "/list")
+    @ResponseBody
+    public Result<List<HouseOrderEvaluationsVO>> list(@RequestBody HouseOrderEvaluationsQueryDto queryDto) {
+        return houseOrderEvaluationsService.list(queryDto);
+    }
+
+}
